23fc77b620c4465eb092ba796485c397e30a4c8d
lihuikun <lihuikun@wezonet.com>
2025-05-19 10:29:13 +0800

    feat: 移除JwtService的提供，简化AuthModule的依赖

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,12 @@
 // auth.module.ts
 import { Module } from '@nestjs/common';
 import { AuthGuard } from './auth.guard'; // 引入 AuthGuard
-import { JwtService } from '@nestjs/jwt'; // 引入 JwtService
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [AuthGuard, JwtService], // 提供 AuthGuard 和 JwtService
+  providers: [AuthGuard], // 提供 AuthGuard 和 JwtService
   exports: [AuthGuard], // 导出 AuthGuard 使其他模块可用
 })
-export class AuthModule {}
+export class AuthModule { }
